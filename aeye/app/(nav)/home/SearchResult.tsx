3e699212ef74db7d575144ec9b7a896f5fb7d3d3
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-04-11 16:00:09 +0900

    ✨ Faet: search reqests

--- aeye/app/(nav)/home/SearchResult.tsx
+++ aeye/app/(nav)/home/SearchResult.tsx
@@ -6,8 +6,20 @@ import { useEffect } from "react";
 
 export default function SearchResult() {
   const searchQuery = useRecoilValue(searchQueryState);
+  const fetchResults = async () => {
+    const res = await fetch(`https://api.a-eye.live/video/search?keyword=${searchQuery}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    });
+    if (res.ok) {
+      const jsonData = await res.json();
+      console.log(jsonData.data);
+    }
+  }
   useEffect(() => { 
-    console.log(searchQuery)
+    fetchResults();
   }, [searchQuery]);
   return (
     <>
