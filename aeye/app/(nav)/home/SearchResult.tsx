c1e2590255d25e6e568a68b7bf0b463e8b95eb5b
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-04-13 13:53:36 +0900

    ✨ Feat: iterate the results

--- aeye/app/(nav)/home/SearchResult.tsx
+++ aeye/app/(nav)/home/SearchResult.tsx
@@ -1,29 +1,46 @@
 "use client";
-import Vidpane from "@/app/components/Vidpane";
 import { useRecoilValue } from "recoil";
 import { searchQueryState } from "@/app/recoil-states";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Vidpane from "@/app/components/Vidpane";
+import { Grid, Paper } from "@mui/material";
+
+function Vidgroup({ videos }: { videos: VideoDocument[] }) {
+  return (
+    <Grid container spacing={2}>
+      {videos.map((video, index) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+          <Paper style={{ padding: 10, background: "#f0f0f0" }}>
+            <Vidpane video={video} />
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
 
 export default function SearchResult() {
   const searchQuery = useRecoilValue(searchQueryState);
+  const [results, setResults] = useState<Vidarr>();
   const fetchResults = async () => {
-    const res = await fetch(`https://api.a-eye.live/video/search?keyword=${searchQuery}`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    });
+    const res = await fetch(
+      `https://api.a-eye.live/video/search?keyword=${searchQuery}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }
+    );
     if (res.ok) {
       const jsonData = await res.json();
       console.log(jsonData.data);
+      setResults(jsonData.data);
     }
-  }
-  useEffect(() => { 
+  };
+  useEffect(() => {
     fetchResults();
-  }, [searchQuery]);
-  return (
-    <>
-      <Vidpane />
-    </>
-  );
+  }, []);
+
+  return <>{results && <Vidgroup videos={results.videoDocuments} />}</>;
 }
--- aeye/app/components/Vidpane.tsx
+++ aeye/app/components/Vidpane.tsx
@@ -1,3 +1,11 @@
-export default function Vidpane() {
-  return <div>video</div>;
+export default function Vidpane({ video }: { video: VideoDocument }) {
+  return (
+    <div>
+      <video controls>
+        <source src={video.content} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <p>{video.id}</p>
+    </div>
+  );
 }
