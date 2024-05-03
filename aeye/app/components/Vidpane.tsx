5bb5ad4e510a3bcd4ac2a7c6045aac287524c730
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-05-03 21:23:49 +0900

    🚧 WIP: 이게 맞나

--- aeye/app/components/Vidpane.tsx
+++ aeye/app/components/Vidpane.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Backdrop from "@mui/material/Backdrop";
 import { styled } from "@mui/material/styles";
 import VideoInfos from "./VidInfos";
@@ -23,9 +23,32 @@ const Video = styled("video")({
   maxHeight: "60%",
 });
 
+function timeStringToSeconds(timeString: string) {
+  const timeParts = timeString.split(":").map(Number);
+  let hours = 0,
+    minutes = 0,
+    seconds = 0;
+
+  if (timeParts.length === 2) {
+    // Format: mm:ss
+    minutes = timeParts[0];
+    seconds = timeParts[1];
+  } else if (timeParts.length === 3) {
+    // Format: hh:mm:ss
+    hours = timeParts[0];
+    minutes = timeParts[1];
+    seconds = timeParts[2];
+  } else {
+    throw new Error("Invalid time string format");
+  }
+
+  return hours * 3600 + minutes * 60 + seconds;
+}
+
 export default function Vidpane({ video }: { video: VideoDocument }) {
   const [showVideoInfo, setShowVideoInfo] = useState(false);
-  const [startTime, setStartTime] = useState(0);
+  const [thumbnailSrc, setThumbnailSrc] = useState<string | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleClick = () => {
     setShowVideoInfo(true);
@@ -35,25 +58,39 @@ export default function Vidpane({ video }: { video: VideoDocument }) {
     setShowVideoInfo(false);
   };
 
-  const handleVideoLoaded = () => {
-    if (startTime > 0) {
-      const videoElement = document.getElementById(
-        "videoPlayer"
-      ) as HTMLVideoElement;
-      if (videoElement) {
-        videoElement.currentTime = startTime;
-        setStartTime(0); // Reset start time after setting it
+  const generateThumbnail = async (timeStamp: number) => {
+    const video = videoRef.current;
+    if (video) {
+      video.crossOrigin = "anonymous";
+      video.pause();
+      video.currentTime = timeStamp;
+      await new Promise((resolve) => video.addEventListener("seeked", resolve));
+      const canvas = document.createElement("canvas");
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const thumbnailSrc = canvas.toDataURL();
+        setThumbnailSrc(thumbnailSrc);
       }
     }
   };
 
+  useEffect(() => {
+    const timeInSeconds = timeStringToSeconds(video.time);
+    generateThumbnail(timeInSeconds);
+  }, []);
+
   return (
     <div>
-      <ThumbnailImage
-        src={video.videoResponseDto.thumbnailUri}
-        alt={video.videoResponseDto.title}
-        onClick={handleClick}
-      />
+      {thumbnailSrc && (
+        <ThumbnailImage
+          src={thumbnailSrc}
+          alt={video.videoResponseDto.title}
+          onClick={handleClick}
+        />
+      )}
       <Backdrop
         open={showVideoInfo}
         onClick={handleClose}
@@ -61,9 +98,9 @@ export default function Vidpane({ video }: { video: VideoDocument }) {
       >
         <VideoContainer>
           <Video
+            ref={videoRef}
             id="videoPlayer"
             controls
-            onLoadedData={handleVideoLoaded}
             src={video.videoResponseDto.videoUri}
           />
           <VideoInfos video={video} />
