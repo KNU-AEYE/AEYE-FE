2c6ceba78c1149df4cbaaf667aec04c23531a819
Jiwoo Ahn <ilp_rincipe@naver.com>
2024-05-19 14:06:12 +0900

    ✨ Feat: tag components

---
+++ aeye/app/(nav)/home/SearchTagList.tsx
@@ -0,0 +1,33 @@
+import { Chip } from "@mui/material";
+import { selectedTagsState } from "@/app/recoil-states";
+import { useRecoilState } from "recoil";
+
+const tags = ["새 객체", "넘어짐", "화재", "자동차", "도로"];
+
+export default function SearchTagList() {
+  const [selectedTags, setSelectedTags] = useRecoilState(selectedTagsState);
+
+  const handleChipClick = (tag: string) => {
+    setSelectedTags((prevSelectedTags) => {
+      if (prevSelectedTags.includes(tag)) {
+        return prevSelectedTags.filter((t) => t !== tag);
+      } else {
+        return [...prevSelectedTags, tag];
+      }
+    });
+  };
+  return (
+    <>
+      {tags.map((tag, index) => (
+        <Chip
+          label={tag}
+          key={index}
+          variant={selectedTags.includes(tag) ? "filled" : "outlined"}
+          sx={{ margin: "0.5vh" }}
+          color={selectedTags.includes(tag) ? "primary" : "default"}
+          onClick={() => handleChipClick(tag)}
+        />
+      ))}
+    </>
+  );
+}
--- aeye/app/(nav)/home/page.tsx
+++ aeye/app/(nav)/home/page.tsx
@@ -2,11 +2,13 @@
 
 import SearchBar from "./SearchBar";
 import SearchResult from "./SearchResult";
+import SearchTagList from "./SearchTagList";
 
 export default function Home() {
   return (
     <>
       <SearchBar />
+      <SearchTagList />
       <SearchResult />
     </>
   );
